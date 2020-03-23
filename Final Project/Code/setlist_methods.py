d3db1c7fcf42b4dab2525b98e7c1588ea08a57ee
aruskin <asruskin@gmail.com>
2020-03-23 17:58:22 -0400

    Minor change to printing in verbose mode

--- Final Project/Code/setlist_methods.py
+++ Final Project/Code/setlist_methods.py
@@ -10,7 +10,7 @@ import numpy as np
 START_DATE = datetime.datetime.strptime('2015-01-01', '%Y-%m-%d')
 END_DATE = datetime.datetime.today()
 
-def get_setlist_events(seed_id, SETLIST_API_KEY, seed_type="artist", verbose=True, limit=100, n_tries=5):
+def get_setlist_events(seed_id, SETLIST_API_KEY, seed_type="artist", verbose=True, limit=100):
   # need to do this first pull outside of loop to get total # events
   page = 1
   if verbose:
@@ -20,13 +20,11 @@ def get_setlist_events(seed_id, SETLIST_API_KEY, seed_type="artist", verbose=Tru
   results = requests.get(request, headers=headers)
   json_results = results.json()
   # Avoid errors from making too many requests in a row
-  tried = 1
-  while ('total' not in json_results.keys()) and (tried < n_tries):
+  while 'total' not in json_results.keys():
     time.sleep(1)
     print("Trying again...")
     results = requests.get(request, headers=headers)
     json_results = results.json()
-    tried += 1
   total_events = json_results['total']
   per_page = json_results['itemsPerPage']
   events = json_results['setlist']
@@ -41,8 +39,7 @@ def get_setlist_events(seed_id, SETLIST_API_KEY, seed_type="artist", verbose=Tru
     request = 'https://api.setlist.fm/rest/1.0/{0}/{1}/setlists?p={2}'.format(seed_type, seed_id, page)
     results = requests.get(request, headers=headers)
     json_results = results.json()
-    tried = 1
-    while ('setlist' not in json_results.keys()) and (tried < n_tries):
+    while 'setlist' not in json_results.keys():
       time.sleep(1)
       print("Trying again...")
       results = requests.get(request, headers=headers)
@@ -51,8 +48,8 @@ def get_setlist_events(seed_id, SETLIST_API_KEY, seed_type="artist", verbose=Tru
     if verbose:
     	print("Fetched {} events".format(len(events)))
   if verbose:
-  	print("")
   	print("\n%d events on  %d pages" % (len(events), page))
+    print("")
   return events
 
 def add_sl_events_to_bipartite_graph(sl_events, G, start_date=START_DATE, end_date=END_DATE):
@@ -65,3 +62,18 @@ def add_sl_events_to_bipartite_graph(sl_events, G, start_date=START_DATE, end_da
       G.add_node(artist_info['mbid'], name=artist_info['name'], \
                      node_type="Artist")
       G.add_edge(artist_info['mbid'], place_info['id'])
+
+def map_sl_event_to_standard(event):
+  new_event = dict()
+  new_event['artist_name'] = event['artist']['name']
+  new_event['artist_mbid'] = event['artist']['mbid']
+  new_event['artist_sl_url'] = event['artist']['url']
+  new_event['time'] = datetime.datetime.strptime(event['eventDate'], '%d-%m-%Y')
+  new_event['venue_sl_name'] = event['venue']['name']
+  new_event['venue_sl_id'] = event['venue']['id']
+  new_event['city_name'] = event['venue']['city']['name']
+  new_event['city_latitude'] = event['venue']['city']['coords']['lat']
+  new_event['city_longitude'] = event['venue']['city']['coords']['long']
+  new_event['event_sl_id'] = event['id']
+  return new_event
+
