4ab7a1667fa14ff776b0db545990011e0a90fb8d
aruskin <asruskin@gmail.com>
2020-04-23 15:10:24 -0400

    Initial Heroku app

--- Final Project/Code/app.py
+++ Final Project/Code/app.py
@@ -67,6 +67,8 @@ external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+server = app.server
+
 app.layout = html.Div([
     html.H1(
         children='Get Artist Recommendations by Tour History',
